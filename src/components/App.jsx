ac49f4c312f2d300cee04f8c9789801281f2b4a4
Lisovyi-Vladyslav <dragodrift2005@gmail.com>
2023-02-13 15:53:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import ReactDOM from "react-dom";
 import { ContactForm } from "components/ContactForm/ContactForm";
 import { Filter } from "components/Filter/Filter";
 import { ContactList } from "components/ContactList/ContactList";
@@ -49,7 +48,6 @@ handleContacts = () => {
 	};
 
   render() {
-    const { contacts, filter } = this.state
     
     return (
       <>
