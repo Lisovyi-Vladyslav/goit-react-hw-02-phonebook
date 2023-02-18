d9f8212fbc2254d33f1be61f1669addaf1422208
Lisovyi-Vladyslav <dragodrift2005@gmail.com>
2023-02-18 12:15:14 +0200

    c

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,36 +1,34 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-import { Component } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-export class Filter extends Component {
-  handleSubmit = (values, actions) => {
-    this.props.handleFilter(values.filter);
-    this.props.handleContacts()
-        actions.resetForm();
-    };
-  render() {
+export const Filter = (props) => {
+  const { register, watch } = useForm({
+    defaultValues: {
+     filter: '',
+    }
+  });
 
+  let filter = watch('filter');
+
+  useEffect(() => {
+    console.log('я працюю')
+    props.handleFilter(filter);
+    props.handleContacts()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter])
     return (
       <>
-        <Formik
-       initialValues={{ filter: ''}}
-       onSubmit={this.handleSubmit}
-        >
-                <Form>
-
-            <h2>Filter</h2>
-           <Field type="text" name="filter" />
-           <ErrorMessage name="filter" component="div" />
-           <button type="submit">
-            Submit
-           </button>
-         </Form>   
-     </Formik>
+        <form >
+        <h2>Filter</h2>
+          <input {...register("filter")} type="text" />
+        </form>
       </>
           
     );
-  }
+  
 }
 
 Filter.propTypes = {
